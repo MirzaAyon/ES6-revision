7f161ea481bc36ca83abe148ac9ef511d2ed3719
MirzaAyon <ayonmirza98@gmail.com>
2022-07-01 20:17:32 +0600

    arrow function added

---
+++ app15.js
@@ -0,0 +1,13 @@
+// without Arrow Function
+function sum(a, b) {
+    let result = a + b
+    return result
+}
+const result = sum(10, 15)
+console.log(result)
+
+
+// with Arrow Function
+const myArrowFunc = (a, b) => a + b
+const newResult = myArrowFunc(34, 90)
+console.log(newResult)
