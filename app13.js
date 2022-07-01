257c606b36c2c79b9821e6eb28d354b71399e3dc
MirzaAyon <ayonmirza98@gmail.com>
2022-07-01 20:02:36 +0600

    inside/outside block added

---
+++ app13.js
@@ -0,0 +1,17 @@
+//inside block outside block
+let x = 20
+{
+    // Area 51
+    let x = 5
+    console.log(x);
+}
+console.log(x); //var er khetre kore dekhbo
+
+var x2 = 20
+{
+    // Area 51
+    var x2 = 5
+    console.log(x2);
+}
+console.log(x2);
+//var er khetre jekhan thekei access korina keno last ta count hbe 
