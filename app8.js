91a8871091351e58c4e2d5dac235c2197b089c67
MirzaAyon <ayonmirza98@gmail.com>
2022-07-01 18:33:28 +0600

    map and templete literal added

---
+++ app8.js
@@ -0,0 +1,16 @@
+//another practice
+// const myArr = ['Naymur', 'Alamin', 'Niloy']
+// let myNewArr = []
+// for (let number of myArr) {
+//     let tripled = number * 3
+//     myNewArr.push(tripled)
+// }
+
+// console.log(myNewArr)
+
+//with map
+
+// const myArr = ['Naymur', 'Alamin', 'Niloy']
+// const tripledArr = myArr.map(name => `Hello ${name}, How are you?`)
+// console.log(tripledArr)
+//map sudhu array te use korte parbo object e parbo na
