0510d8b63f067ceebe68d3d09961ddd0a98849cf
MirzaAyon <ayonmirza98@gmail.com>
2022-07-01 11:48:07 +0600

    arrow function with 3 parameters added

--- app.js
+++ app.js
@@ -33,15 +33,38 @@ console.log(id);
 
 //5.Arrow function
 
-function sum(a, b) {
-    let result = a + b
-    return result
-}
-const result = sum(10, 15)
-console.log(result)
+// function sum(a, b) {
+//     let result = a + b
+//     return result
+// }
+// const result = sum(10, 15)
+// console.log(result)
 //this is a function without arrow
 
-const sum = (a, b) => a + b;
-const result1 = sum(12, 15)
-console.log(result1)
+// const sum = (a, b) => a + b;
+// const result1 = sum(12, 15)
+// console.log(result1)
+//this is a function with arrow
+//with 2 parameters
+
+
+//ichcha korle ekta parameter o dewa jae
+// const sum = a => a
+// const result = sum(10)
+// console.log(result)
+//etao ekta function
+//with 1 parameter
+
+const sum = (a, b, c) => {
+    let sum1 = a + b
+    let sum2 = a + c
+
+    return sum2;
+    // return sum2, sum1; //2 ta dile first jeta declare kora hoise shetai count hbe jemon sum1
+}
+const result = sum(10, 21, 34)
+console.log(result)
+
+
+
 
