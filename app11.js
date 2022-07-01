e63271ca34e1589a88d7ec5fba1af4e7e484fba8
MirzaAyon <ayonmirza98@gmail.com>
2022-07-01 19:34:22 +0600

    find added

---
+++ app11.js
@@ -0,0 +1,18 @@
+//find
+
+
+const newArr2 = ['Faysal', 'Arfan', 'Meraj', 'Tanim']
+
+const foundName = newArr2.find(name => name.length % 2 != 0)
+console.log(foundName);
+
+console.log(...foundName);
+//prothom ta eshe off hoye jabe
+
+
+//find
+const newArr = [1, 2, 3, 4, 53, 5, 6, 7, 8, 9, 10, 45, 82, 63, 98];
+
+const found = newArr.find(number => number > 10);
+console.log(found);
+//fist e 10 er boro jetake pabe otakei show korbe
