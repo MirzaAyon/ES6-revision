f7d941b18b828cb288f88aa514819d5f99ed40c7
MirzaAyon <ayonmirza98@gmail.com>
2022-07-01 11:03:58 +0600

    templete litaral added

--- app.js
+++ app.js
@@ -10,3 +10,23 @@ let hisName = "Mirza";
 hisName = 'minhaz';
 console.log(hisName);
 //eta impossible
+
+Age = 20;
+console.log(Age);
+//etao possible 
+let Age2;
+console.log(Age2); //undefined ashbe
+//etao possible na
+
+
+let id = 10;
+id = 11;
+console.log(id);
+//possible
+//declare na kora geleo , re assign, re define kora jae
+//but const diye ei kaj kora jabe na
+
+
+// 4.templete litaral
+// eta aro name templete string, string templete, backticks syntex ei total 4 ta name etar
+
