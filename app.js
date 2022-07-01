d7eb4d9178db4c4b148d32a899de5fbf34103e73
MirzaAyon <ayonmirza98@gmail.com>
2022-07-01 10:59:18 +0600

    var and let added

---
+++ app.js
@@ -0,0 +1,12 @@
+//let
+var myName = "Ayon";
+var myName = "plabon";
+console.log(myName);
+//eta possible
+
+let hisName = "Mirza";
+// let hisName = "Minhaz";
+//eta impossible
+hisName = 'minhaz';
+console.log(hisName);
+//eta impossible
