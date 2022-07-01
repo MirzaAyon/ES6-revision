ebd002e71c939005cd6b93fc09d7a30959284446
MirzaAyon <ayonmirza98@gmail.com>
2022-07-01 15:11:33 +0600

    default parameter added

---
+++ app7.js
@@ -0,0 +1,10 @@
+// Map
+
+const myArr = [2, 4, 6, 8, 10];
+let myNewArr = []
+for (number of myArr) {
+    let tripled = number * 3;
+    myNewArr.push(tripled)
+}
+console.log(myNewArr);
+//3 gun korte ei code amdr use kora lagto ja map er shahajje aro easy
